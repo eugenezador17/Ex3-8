251de7f9150c031aacb526173eedaefeed570089
eugenezador17 <cpostbox@mail.ru>
2018-10-09 19:30:38 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main(){
+    srand(time(nullptr));
+    cout << time(nullptr) << endl;
+
+    const size_t size = 5;
+    const size_t cols[size] = {3, 7, 6, 4, 2};
+
+    int** nums = new int*[size];
+    cout << nums << endl;
+    for(unsigned int i = 0; i < size; i++){
+        nums[i] = new int[cols[i]];//Выделение памяти под cols[i] intов
+        cout << "| ";
+        for(unsigned int j = 0; j < cols[i]; j++){
+            //*(*(nums + i) + j) = rand()%10;
+            nums[i][j] = rand()%10;
+            cout << nums[i][j] << " | ";
+        }
+        cout << endl;
+    }
+    for(unsigned int i = 0; i < size; i++){
+        delete[] nums[i];
+    }
+    delete[] nums;
+    system("pause");
+    return 0;
+}
